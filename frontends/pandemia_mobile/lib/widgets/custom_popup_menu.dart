1d910183d749f92720f09d48a5fa30199bc6a3cd
Ahmad Rifa'i <arifai209@gmail.com>
2020-04-12 02:19:19 +0700

    Implementasi screen untuk update profile satgas

---
+++ frontends/pandemia_mobile/lib/widgets/custom_popup_menu.dart
@@ -0,0 +1,6 @@
+class CustomPopupMenu {
+  static const String profile = 'Edit Profil Satgas';
+  static const String about = 'Tentang';
+
+  static const List<String> choices = <String>[profile, about];
+}
